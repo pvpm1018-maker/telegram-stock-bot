45e37a2ab5c60181a90a238017eb253cfdc4454b
pvpm1018-maker <pvpm1018@gmail.com>
2026-07-14 16:26:05 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,25 @@
-print("안녕하세요!")
-print("주식봇 설치를 시작합니다.")
+import requests
+from config import BOT_TOKEN, CHAT_ID
+
+message = """🎉 JH Portfolio AI Bot
+
+연결 테스트 성공!
+
+이 메시지가 보인다면
+GitHub와 텔레그램이 정상적으로 연결되었습니다.
+
+다음 단계부터는
+📈 주식 브리핑 기능을 추가합니다.
+"""
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print(response.text)
